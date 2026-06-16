1b78e5261d5004d5540bcdbbe3faa6489722adb6
Yehor Smoliakov <yehors@ukr.net>
2026-06-16 14:32:09 +0000

    use C++ features

---
+++ include/morlet.h
@@ -0,0 +1,35 @@
+//
+// Created by jonas on 29.02.24.
+//
+
+#pragma once
+
+#include <complex>
+#include <vector>
+
+#include "wavelet.h"
+#include "api.h"
+
+namespace fcwt {
+    class Morlet: public Wavelet {
+        public:
+        FCWT_LIBRARY_API explicit Morlet(float bandwidth); // frequency domain
+
+        ~Morlet() override = default;
+
+        // Frequency domain
+        void generate(int size) noexcept override;
+
+        // Time domain
+        void generate(std::vector<std::complex<float>> &pwav, int size, float scale) noexcept override;
+
+        [[nodiscard]] int getSupport(float scale) const noexcept override;
+
+        void getWavelet(float scale, std::vector<std::complex<float>>& pwav, int pn) noexcept override;
+
+        float fb;
+
+        private:
+        float ifb, fb2;
+    };
+}
---
+++ justfile
@@ -0,0 +1,2 @@
+format:
+    clang-format -i include/*.h src/*.cpp
